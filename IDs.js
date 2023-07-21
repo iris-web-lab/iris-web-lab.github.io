09ddf0abc16e9363125135be5d5b65338b026025
Luka Djordjevic <lukadordevi@gmail.com>
2023-07-21 14:53:49 +0200

    optimized

--- IDs.js
+++
@@ -1,4 +0,0 @@
-var sheetId = "109zQw4KjhYl4UneLxvPRLINLHriIw8MwtdGx8Bvk4LY";
-
-var sheetBlog = "https://docs.google.com/spreadsheets/d/"sheetId"/gviz/tq?tqx=out:json&sheet=blog"
-var sheetProduct = "https://docs.google.com/spreadsheets/d/"sheetId"/gviz/tq?tqx=out:json&sheet=products"
---
+++ resources/IDs.js
@@ -0,0 +1,5 @@
+var sheetId = "109zQw4KjhYl4UneLxvPRLINLHriIw8MwtdGx8Bvk4LY";
+
+var sheetBlog = "https://docs.google.com/spreadsheets/d/" + sheetId + "/gviz/tq?tqx=out:json&sheet=blog";
+var sheetProduct = "https://docs.google.com/spreadsheets/d/" + sheetId + "/gviz/tq?tqx=out:json&sheet=products";
+
