b8097366fbaa5b3df4128e521b0d3591df4f4f2f
samolukadjo <82143316+samolukadjo@users.noreply.github.com>
2023-07-21 14:12:18 +0200

    Update IDs.js

--- IDs.js
+++ IDs.js
@@ -1,4 +1,4 @@
 var sheetId = "109zQw4KjhYl4UneLxvPRLINLHriIw8MwtdGx8Bvk4LY";
 
-var sheetBlog = 
-var sheetProduct =
+var sheetBlog = "https://docs.google.com/spreadsheets/d/"sheetId"/gviz/tq?tqx=out:json&sheet=blog"
+var sheetProduct = "https://docs.google.com/spreadsheets/d/"sheetId"/gviz/tq?tqx=out:json&sheet=products"
